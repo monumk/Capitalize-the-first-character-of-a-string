c0f831fc0b56a279ff18182c413ccd3af0c207ac
monumk <73218094+monumk@users.noreply.github.com>
2021-06-26 23:27:18 +0530

    Add files via upload

---
+++ capa.py
@@ -0,0 +1,11 @@
+'''Capitalize the first character of a string'''
+
+str1='florida dolphins'
+print(str1.capitalize())
+
+#output Florida dolphins
+
+str2='delhi public school'
+print(str2.capitalize())
+
+#output Delhi public school
